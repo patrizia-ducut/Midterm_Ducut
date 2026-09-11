8c513761b125c5ea0e25c2fbbed98fece2460891
patrizia-ducut <patriziaducut615@gmail.com>
2026-09-11 21:06:17 +0800

    Push 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        parchment: "#F7F3EC",
+        walnut: "#241A14",
+        saddle: "#8B4513",
+        brass: "#C9A15A",
+        fretboard: "#2E2A26",
+        rust: "#B23B2E",
+      },
+      fontFamily: {
+        display: ["Fraunces", "serif"],
+        body: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
